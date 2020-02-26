a7796f64634b1baf68a9769e9e7a6becbc681302
rajon hossain <rajonhossaindhaka@gmail.com>
2020-02-26 15:14:59 +0600

    sdfsd

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var cors = require('cors')
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 // var bodyParser = require('body-parser')
 var connection = require('../dbconnect');
+var cors = require('cors');
+var bodyParser = require('body-parser')
 
  
 
@@ -11,17 +13,30 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.get('/learners' , (req, res) => {
-	// console.log('conn=',connection);
-	connection.query('SELECT * FROM react_table', (err, rows, fields) => {
 
-		console.log(rows);
+router.post('/output2', function(req, res){
+    var name = req.body.name;
+    var fname = req.body.fname;
+    var mname = req.body.mname;
 
-		if (!err)
-			res.send(rows);
-		else
-			console.log(err);
+    console.log(name,fname,mname)
+
+    // console.log("......." + name);
+
+    var sql = "INSERT INTO react_table (name, fname, mname) VALUES(\""+name+"\",\""+fname+"\",\""+mname+"\")";
+  	connection.query(sql, function (err, result) {
+    	res.send({id:result.insertId,name:name,fname:fname,mname:mname});
 	});
 });
+
+router.get('/learners', cors(), function (req, res, next) {
+	connection.query('SELECT * FROM react_table', (err, rows, fields) => {
+	if (!err){
+			 res.send(rows);
+		}
+	});
+});
+
+
 
 module.exports = router;
